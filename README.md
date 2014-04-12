33a422411aef0726a9c65f47acaa1301f385b795
Kyle Drake <kyle@kyledrake.net>
2014-04-12 02:11:22 -0700

    refactor a bit, add delete and info calls

--- README.md
+++ README.md
@@ -9,17 +9,42 @@ A node.js library for interacting with the [NeoCities](https://neocities.org/api
 
 ## Usage
 
+### Uploading files
+
 ``` javascript
 var NeoCities = require('neocities')
-
 var api = new NeoCities('YOURUSERNAME', 'YOURPASSWORD')
 
-// Upload a file called local.html from your computer, which will be named newfile.html on the site.
+// local file is index.js, saved on site as derp.js
 
-api.uploadFile('newfile.html', './local.html', function(resp) {
-  if(resp.result == 'error')
-    throw new Error(resp.error_type+' - '+resp.message)
+api.upload([
+  {name: 'derp.js', path: './index.js'}
+], function(resp) {
+  console.log(resp)
+})
+```
 
+### Deleting files
+
+``` javascript
+var NeoCities = require('neocities')
+var api = new NeoCities('YOURUSERNAME', 'YOURPASSWORD')
+
+api.delete(['derp.js'], function(resp) {
+  console.log(resp)
+})
+```
+
+### Get site info (hits, et cetera)
+
+``` javascript
+api.info(function(resp) {
+  console.log(resp)
+})
+```
+
+``` javascript
+api.info('youpi', function(resp) {
   console.log(resp)
 })
 ```
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var http     = require('http')
 var https    = require('https')
 var path     = require('path')
 var url      = require('url')
+var qs       = require('querystring')
 var FormData = require('form-data')
 
 function NeoCities(user, pass, opts) {
@@ -10,21 +11,59 @@ function NeoCities(user, pass, opts) {
   this.pass = pass
   this.opts = opts || {}
   this.url = url.parse(this.opts.url || 'https://neocities.org')
+  this.client = this.url.protocol == 'https:' ? https : http
 }
 
-NeoCities.prototype.uploadFiles = function(files, callback) {
+NeoCities.prototype.get = function(method, args, callback) {
+  var path = '/api/'+method
+
+  if(args)
+    path += '?'+qs.stringify(args)
+
+  var request = this.client.request({
+    method: 'get',
+    host: this.url.hostname,
+    port: this.url.port,
+    path: path,
+    auth: this.user+':'+this.pass
+  }, function(res) {
+    var body = ''
+
+    res.on('data', function (chunk) {
+      body += chunk
+    })
+
+    res.on('end', function() {
+      var resObj = JSON.parse(body)
+      callback(resObj)
+    })
+  })
+  request.end()
+}
+
+NeoCities.prototype.info = function(sitename, callback) {
+  var args = null
+
+  if(typeof sitename == 'function')
+    callback = sitename
+  else if(typeof sitename == 'string')
+    args = {sitename: sitename}
+
+  this.get('info', args, callback)
+}
+
+NeoCities.prototype.post = function(method, args, callback) {
   var form = new FormData()
+  var i
 
-  for(i=0;i<files.length;i++)
-    form.append(files[i].name, fs.createReadStream(files[i].path))
+  for(i=0;i<args.length;i++)
+    form.append(args[i].name, args[i].value)
 
-  var client = this.url.protocol == 'https:' ? https : http
-
-  var request = client.request({
+  var request = this.client.request({
     method: 'post',
     host: this.url.hostname,
     port: this.url.port,
-    path: '/api/upload',
+    path: '/api/'+method,
     headers: form.getHeaders(),
     auth: this.user+':'+this.pass
   }, function(res) {
@@ -43,8 +82,24 @@ NeoCities.prototype.uploadFiles = function(files, callback) {
   form.pipe(request)
 }
 
-NeoCities.prototype.uploadFile = function(name, path, callback) {
-  this.uploadFiles([{name: name, path: path}], callback)
+NeoCities.prototype.delete = function(filenames, callback) {
+  var args = []
+  var i
+
+  for(i=0;i<filenames.length;i++)
+    args.push({name: 'filenames[]', value: filenames[i]})
+
+  this.post('delete', args, callback)
+}
+
+NeoCities.prototype.upload = function(files, callback) {
+  var args = []
+  var i
+
+  for(i=0;i<files.length;i++)
+    args.push({name: files[i].name, value: fs.createReadStream(files[i].path)})
+
+  this.post('upload', args, callback)
 }
 
 module.exports = NeoCities
